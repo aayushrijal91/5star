752237100236137122882d49cb5112c1fac2a77a
Aayush Rijal <arijal@aiims.com.au>
2024-05-28 12:54:32 +1000

    map update

--- footer.php
+++ footer.php
@@ -146,7 +146,7 @@
 
                 <div class="col-12 col-md-4 col-lg-6 col-xl-2 d-none d-md-block">
                     <div class="h-100 rounded-20 overflow-hidden">
-                        <iframe class="m-0" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13260.628432027534!2d151.01220619999998!3d-33.80825850000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sau!4v1712878687714!5m2!1sen!2sau" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe class="m-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6630.6038443650605!2d151.0010588764376!3d-33.80452081573372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12a3031e633ddd%3A0x82f3003f581e257b!2s3%20Harold%20St%2C%20Parramatta%20NSW%202150!5e0!3m2!1sen!2sau!4v1716864111719!5m2!1sen!2sau" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
